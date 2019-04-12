55a30c9baaf754d6995e7d577e5267be8cdbf83e
Aaron <aaronrbeck@gmail.com>
2019-04-12 10:15:41 -0600

    added a bunch of inline notes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React, { useState, 
-                
+import React, { useState,           
               Fragment } from 'react';
+
+//Import components to App
 import UserTable from './tables/UserTable'
 import AddUserForm from './forms/AddUserForm'
 import EditUserForm from './forms/EditUserForm'
 
 
+
+//with hooks, the component is no longer a class that extends
+//components are now just giant functions
 const App = () => {
-  //Data
+  //Dummy data for this project:
+  //The real question is how would we fetch, or graphql real data into a reach project?
   const usersData = [
     { id: 1, name: 'Tania', username: 'floppydiskette' },
     { id: 2, name: 'Craig', username: 'siliconeidolon' },
     { id: 3, name: 'Ben', username: 'benishpere' },
   ]
 
-  //until user is selected, the initial form state is empty:
+  //until user is selected, the initial form state is empty,
+  //Is initialFormState an acutal React word or something we just invented?
+  //I feel like initialFormState is a React word, but I have yet to 
+  //track it down in their documentation
   const initialFormState = { id: null, name: '', username: '' }
 
 
   //Next 3 entries: Setting state
+  //useState below is a React Hook function word
+//useState  is a new way to use the exact same capabilities that 
+//this.state provides in a class. The only argument to the 
+//useState() Hook is the initial state. Unlike with classes, the state
+//doesn’t have to be an object. What does useState return? 
+//It returns a pair of values: the current state and a function that 
+//updates it.This is why we write const [count, setCount] = useState().
+//This is similar to this.state.count and this.setState in a class, 
+//except you get them in a pair.  
+//[users, setUsers] below are not React words - use any words that make sense to you
+//we have initialized the state users with (usersData), in Hook
+// documentation they use the 2nd returned item (setUsers) in my case
+// as a function. not doing that here, but it does show up in 
+// each CRUD operation below   
 const [users, setUsers] = useState(usersData)
   //see and update current user being edited, so apply empty
   //user to a currentUser state:
@@ -30,11 +52,15 @@ const [editing, setEditing] = useState(false)
 
 //CRUD operations:
   const addUser = user => {
+    //I'm not sure where id comes from in the next line
     user.id = users.length + 1
+    //and then user becomes the 2nd returned item?
     setUsers([...users, user])
   }
 
   const deleteUser = id => {
+    //ok, so setEditing is the 2nd returned item, what is 
+    //this doing again? setting it to false
     setEditing(false)
 
     setUsers(users.filter(user => user.id !== id))
@@ -69,7 +95,9 @@ const editRow = user =>{
         <div className="flex-row">
           <div className = "flex-large">
 {/* create a toggle with ternary if edting state is t/f : show edit/show add
-   */}       {editing ? ( 
+   what the hell is {editing ? i do understand if editing is true, then 
+   display the edituserform, otherwise display the add user fragment*/}       
+            {editing ? ( 
             <Fragment>
               <h2>Edit User</h2>
               <EditUserForm
--- src/forms/EditUserForm.js
+++ src/forms/EditUserForm.js
@@ -7,7 +7,7 @@ const EditUserForm = props =>{
 
     //I'm not compiling, this useEffect is undefined?  why?
     //figured it out, failed to bring useEffect in during the import
-    //statement
+    //statement.  I'm not following the function that follows
     useEffect(
         () => {
         setUser(props.currentUser)
@@ -16,10 +16,10 @@ const EditUserForm = props =>{
      )
 
 
-
+//If I remember correclty, this is what makes on change events update to the state
 const handleInputChange = event => {
     const { name, value } = event.target
-
+//I don't understand the spread operator on users, spread users. hmm
     setUser({...user, [name]:value })
 }
 
@@ -30,7 +30,9 @@ return (
     <form 
     onSubmit={event => {
         event.preventDefault()
-
+//where did updateUser come from?  It is the function that we wrote
+//over in app.js, so state from App somehow shuffled over and made 
+//one of its functions available to us in this new component?
         props.updateUser(user.id, user)
     }}
     >
