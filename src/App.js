08acee19feaa940565633a7ebf50fd970c4bcbb7
Aaron <aaronrbeck@gmail.com>
2019-04-11 18:45:27 -0600

    added UserTable component to App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
+import UserTable from './tables/UserTable'
 
 const App = () => {
   return(
     <div className="container">
-    <h1>CRUD app with Hooks</h1>
-    <div className="flex-row">
-    <div className = "flex-large">
-    <h2>Add User</h2>
-    </div>
-    <div className="flex-large">
-    <h2>View Users</h2>
-    </div>
-    </div>
+      <h1>CRUD app with Hooks</h1>
+        <div className="flex-row">
+          <div className = "flex-large">
+            <h2>Add User</h2>
+          </div>
+          <div className="flex-large">
+            <h2>View Users</h2>
+            <UserTable />
+          </div>
+      </div>
     </div>
   )
 }
